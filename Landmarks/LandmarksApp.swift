0c4707d8ae2acf3d2e8013a03e6c5de3bfe05d0a
Sam Neale <sammneale@icloud.com>
2023-05-31 14:36:23 +1000

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Sam Neale on 31/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
